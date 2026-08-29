bac04854521e66c67eced7e86a42755ac37bb709
Fatima-Kattan <fatimaqattan959@gmail.com>
2026-08-29 17:47:21 +0300

    edit sidebar and deleted files

--- src/app/(dashboard)/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-8">
-      <main className="flex flex-col gap-8 items-center text-center">
-      </main>
-    </div>
-  );
-}
